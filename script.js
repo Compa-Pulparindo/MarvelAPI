8809de9ae66a8249dfc03c8c8b47314598bd5dfa
Compa-Pulparindo <rivera.lopez.p.prog@gmail.com>
2023-12-05 01:55:31 -0700

    CSS y pequeñas mejoras

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
 const newAvengers = [];
 
-const select = document.getElementById('universe');
+const select = document.getElementById("universe");
 
 //Eventos
 document.getElementById("search").addEventListener("click", async () => {
-displaycharacter();
-resetInput();
-});
-
-document.getElementById("new-team").addEventListener("click", () => {
-disableElements(false);
-cleanTeam();
+  displaycharacter();
+  resetInput();
+  cleanUniverse();
 });
 
 document.getElementById("clear-history").addEventListener("click", () => {
-clearHistoy();
+  clearHistoy();
 });
 
 //Funcion de mensaje
 function showAlert(message) {
-document.getElementById("alert").innerHTML = "";
-const Alert = document.getElementById("alert");
-const element = document.createElement("div");
-element.innerHTML = `
+  document.getElementById("alert").innerHTML = "";
+  const Alert = document.getElementById("alert");
+  const element = document.createElement("div");
+  element.innerHTML = `
       <h2>${message}</h2>
   `;
-Alert.appendChild(element);
+  Alert.appendChild(element);
 }
 
 //Funciones manipulan a los eventos y a componentes del DOM
 function resetInput() {
   document.getElementById("character").value = "";
-}
-
-function cleanTeam() {
-  document.getElementById("character-container").innerHTML = "";
-  document.getElementById("alert").innerHTML = "";
-  document.getElementById("new-team").disabled = true;
 }
 
 function clearHistoy() {
@@ -47,6 +37,10 @@ document.getElementById("show-history").addEventListener("click", () => {
   displayCharacterHistory();
 });
 
+function cleanUniverse() {
+  document.getElementById("universe-container").innerHTML = "";
+}
+
 function disableElements(status) {
   document.getElementById("character").disabled = status;
   document.getElementById("search").disabled = status;
@@ -54,14 +48,14 @@ function disableElements(status) {
 
 //Funcion que
 async function displaycharacter() {
-const characterName = document.getElementById("character").value;
-if (!characterName) {
-  showAlert("Favor de ingresar un nombre de un personaje");
-  return;
-}
-const character = await searchCharacters(characterName);
-addCharacter(character);
-selectUniverse(character);
+  const characterName = document.getElementById("character").value;
+  if (!characterName) {
+    showAlert("Favor de ingresar un nombre de un personaje");
+    return;
+  }
+  const character = await searchCharacters(characterName);
+  addCharacter(character);
+  selectUniverse(character);
 }
 
 //Función fetch
@@ -105,14 +99,16 @@ function addCharacter(character) {
   characters.forEach((character) => {
     const characterElement = document.createElement("div");
     characterElement.innerHTML = `
-                <div class="characMarvel">
+                <div class="characterMarvel">
                     <h2>${character.name}</h2>
                   <div class="portada-marvel">
                     <img class="imgTa" src="${character.thumbnail.path}.${
       character.thumbnail.extension
     }">
                   </div>
-                    <p class="characDescription">${character.description || "No hay descripción"}</p>
+                    <p class="characterDescription">${
+                      character.description || "No hay descripción"
+                    }</p>
                 </div>
                 `;
     charactersList.appendChild(characterElement);
@@ -132,7 +128,7 @@ function displayCharacterHistory() {
     element.innerHTML = `
             <div class="history-character">
                 <div class="col">
-                  <strong>Name: </strong> ${character.data.results[0].name}
+                  <strong>Name: ${character.data.results[0].name}</strong> 
                   <img class="img-history" src="${character.data.results[0].thumbnail.path}.jpg" width="150px" height="150px">
                 </div>
             </div>          
@@ -141,18 +137,19 @@ function displayCharacterHistory() {
   });
 }
 
-  function selectUniverse(character) {
-    const characterList = document.getElementById("universe-container");
-    const universe = document.getElementById("universe").value;
-    const element = document.createElement("div");
-    const selectedCharacter = character.data.results[0];
-    const selectedUniverse = selectedCharacter[universe].items;
-    selectedUniverse.forEach((items) => {
-      element.innerHTML += `
-      <div>
+function selectUniverse(character) {
+  const characterList = document.getElementById("universe-container");
+  const universe = document.getElementById("universe").value;
+  const element = document.createElement("div");
+  element.classList.add("list-group");
+  const selectedCharacter = character.data.results[0];
+  const selectedUniverse = selectedCharacter[universe].items;
+  selectedUniverse.forEach((items) => {
+    element.innerHTML += `
+      <div class="universe-container-list">
       <li class="universe-list">${universe}: ${items.name}</li>
       <div> 
         `;
-    });
-    characterList.appendChild(element);
-  }
+  });
+  characterList.appendChild(element);
+}
